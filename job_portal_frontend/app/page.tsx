c7e0f055ecc5f60a55909037f354579f7269c95a
Andresh4321 <shahiandy123@gmail.com>
2026-07-10 17:06:34 +0545

    Improve landing page layout

--- job_portal_frontend/app/page.tsx
+++ job_portal_frontend/app/page.tsx
@@ -5,8 +5,12 @@ import VerifyPage from "./Features/Register/page";
 import SalaryExplorerPage from "./Features/SalaryExplorer/page";
 import PostJobPage from "./Features/PostJob/page";
 import FindJobsPage from "./Features/Findjob/page";
-import JobProfilePage from "./Features/JobProfile/page";
+import JobProfilePage from "./Features/JobProfile/[jobId]/page";
+import LoginPage from "./Features/login/page";
+import EmployerDashboard from "./Features/employer_dashboard/page";
+import ApplicationsPage from "./Features/Applications/page";
+import ApplicantsListPage from "./Features/ApplicantsList/page";
 
 export default function Home() {
-  return <FindJobsPage/>;
+  return <Homepage/>;
 }
